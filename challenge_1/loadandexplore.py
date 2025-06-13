f654ebae3a26ff598b7ef62131cd0985ad632fca
cubeforme <g.vmanstein@outlook.de>
2025-06-13 15:22:56 +0200

    Functioning prediction and submission mechanism

--- challenge_1/loadandexplore.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-train = pd.read_csv('../data/challenge_1/train/classification_data.csv')
-val = pd.read_csv('../data/challenge_1/val/classification_data.csv')
-
-print(train.head())
-print(train['responsible_entity_level'].value_counts())
-print(train['category'].value_counts())
